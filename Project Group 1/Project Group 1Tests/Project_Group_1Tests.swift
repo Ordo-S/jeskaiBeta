52ba6d802d74b6be43344a2f67d835dbbc1b6f90
Ordo <matt@spadarolabs.com>
2018-09-21 22:37:09 -0700

    Fix

---
+++ Project Group 1/Project Group 1Tests/Project_Group_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Project_Group_1Tests.swift
+//  Project Group 1Tests
+//
+//  Created by Ordo on 9/20/18.
+//  Copyright © 2018 Group1. All rights reserved.
+//
+
+import XCTest
+@testable import Project_Group_1
+
+class Project_Group_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
