2f56fadd461e292c0f9023d3528b0a4e1bd25955
Mark Casapao <casapao.markkenneth@gmail.com>
2018-10-20 14:46:45 -0700

    Implemented preliminary login page
    The login page successfully implements login using Firebase. It currently doesn't look good with UI, and would require the user to remember their password successfully.

---
+++ AreYouFreeV0.1/AreYouFree/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  AreYouFree
+//
+//  Created by Mark Casapao on 9/20/18.
+//  Copyright © 2018 SJSU CMPE137. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+import Firebase
+
+class ViewController: UIViewController {
+
+    var ref: DatabaseReference!
+    @IBOutlet weak var loginSelector : UISegmentedControl!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        ref = Database.database().reference()
+    }
+    
+    func updateViewFromModel() {
+        
+    }
+    
+    //Activates when you change the value of the selector
+    @IBAction func loginSelectorChanged(_ sender: UISegmentedControl) {
+        let selectedIndex = loginSelector.selectedSegmentIndex //finds the index of selector
+        switch selectedIndex {
+        case 0: //0 means sign in
+            loginLabel.text = "Please Sign In."
+            signInButton.setTitle("Sign In", for: .normal)
+        case 1: //1 means register
+            loginLabel.text = "Please Register."
+            signInButton.setTitle("Register", for: .normal)
+        default:
+            print("Somehow you chose a button in the selector that doesn't exist!")
+        }
+    }
+    
+    @IBAction func signInClicked(_ sender: UIButton) {
+        //Check if email and password are filled in
+        if let email = emailTextField.text, let pass = passwordTextField.text {
+            //Check if you're doing sign in or register
+            let selectedIndex = loginSelector.selectedSegmentIndex //finds the index of selector
+            switch selectedIndex {
+            case 0: //0 means sign in
+                Auth.auth().signIn(withEmail: email, password: pass) { (user, error) in
+                    //check user sign in is successful
+                    if let userChecked = user {
+                        //Show homepage
+                        self.performSegue(withIdentifier: "goToHome", sender: self) //we use self. because this is inside a closure
+                    } else {
+                        //Show error
+                        self.loginLabel.text = "Sorry, login failed."
+                    }
+                }
+            case 1: //1 means register
+                Auth.auth().createUser(withEmail: email, password: pass) { (user, error) in
+                    //check user creation is successful
+                    if let userChecked = user {
+                        //Show confirmed creation
+                        self.performSegue(withIdentifier: "goToHome", sender: self) //we use self. because this is inside a closure
+                    } else {
+                        //Show error
+                        self.loginLabel.text = "Sorry, registration failed."
+                    }
+                }
+            default:
+                print("Somehow you chose a button in the selector that doesn't exist!")
+            }
+        }
+    }
+    
+}
+
---
+++ AreYouFreeV0.1/Pods/Target Support Files/Pods-AreYouFreeTests/Pods-AreYouFreeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AreYouFreeTests : NSObject
+@end
+@implementation PodsDummy_Pods_AreYouFreeTests
+@end
