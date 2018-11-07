e92869a512f38f8de6b5cdf3f18082210989972a
Ordo <matt@spadarolabs.com>
2018-11-07 13:35:15 -0800

    added in basic data model

--- areYouFreev2/areYouFreev2/Model/event.swift
+++ areYouFreev2/areYouFreev2/Model/event.swift
@@ -6,5 +6,24 @@
 //  Copyright © 2018 Group 1. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class event{
+    //MARK: Properties
+    var name: String
+    var photo: UIImage?
+    //Mark: Inits
+    // Initialization should fail if there is no name.
+    init?(name:String, photo:UIImage) {
+        // Initialize stored properties.
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        self.name = name
+        self.photo = photo
+        
+    }
+}
 
