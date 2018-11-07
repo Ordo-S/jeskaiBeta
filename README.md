d1cc84d5a786b8798afc680dcea52eec54efb2df
Ordo <matt@spadarolabs.com>
2018-11-07 11:02:38 -0800

    Fire is cleansing and this was hot garbage, you didnt even see the codebase beofore i nuked it and now you never will

--- README.md
+++
@@ -1,4 +0,0 @@
-jeskaiBeta
-
-
-removed old swift files from past assingment addeing in new file for are you free alpha files
---
+++ areYouFreev2/areYouFreev2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  areYouFreev2
+//
+//  Created by Matt Spadaro on 11/7/18.
+//  Copyright © 2018 Group 1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
