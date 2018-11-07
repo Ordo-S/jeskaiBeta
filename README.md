f0e985b4d6776708ecf2bf619bd4f5baeb59fa2c
Ordo <matt@spadarolabs.com>
2018-11-07 11:32:23 -0800

    Base layout setup with outlets set up in code, added in stackview and set up basic autolayout

---
+++ README.md
@@ -0,0 +1,4 @@
+# Are you free V2 
+
+Whats up, bassically i nuked the whole code base on 11/7 beacuse it was hot garbage and needed to be started over. Will try to activly commit new chages this time
+
--- areYouFreev2/areYouFreev2/ViewController.swift
+++ areYouFreev2/areYouFreev2/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Properties of View
+    @IBOutlet weak var eventTextField: UITextField!
+    @IBOutlet weak var eventNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    //Actions of View
+    @IBAction func setDefualtLabelText(_ sender: UIButton) {
+        eventNameLabel.text = "Default Text"
+    }
+   
 
 }
 
