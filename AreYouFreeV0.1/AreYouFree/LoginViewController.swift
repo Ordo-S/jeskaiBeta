c296aa0d23949dafa3bfe53d2b4278b9170d83a9
Mark Casapao <casapao.markkenneth@gmail.com>
2018-11-12 17:35:17 -0800

    Added validation
    Added validation so that the user knows what's wrong whenever they make a mistake during login or registration.

--- AreYouFreeV0.1/AreYouFree/LoginViewController.swift
+++ AreYouFreeV0.1/AreYouFree/LoginViewController.swift
@@ -65,34 +65,59 @@ class LoginViewController: UIViewController {
             //Check if you're doing sign in or register
             let selectedIndex = loginSelector.selectedSegmentIndex //finds the index of selector
             switch selectedIndex {
+                
             case 0: //0 means sign in
                 Auth.auth().signIn(withEmail: email, password: pass) { (user, error) in
-                    //check user sign in is successful
-                    if let userChecked = user {
-                        //Show homepage
-                        self.performSegue(withIdentifier: "goToHome", sender: self) //we use self. because this is inside a closure
+                    //Validation begins here
+                    if (email.count == 0 || pass.count == 0){
+                        self.loginLabel.text = "Email/password fields can't be empty."
+                    } else if error != nil {
+                        //Handle error
+                        if let errCode = AuthErrorCode(rawValue: error!._code) {
+                            switch errCode {
+                            case .invalidEmail:
+                                self.loginLabel.text = "Invalid email."
+                            case .wrongPassword:
+                                self.loginLabel.text = "Incorrect password."
+                            default:
+                                print("User Error: \(error!)")
+                            }
+                        }
                     } else {
-                        //Show error
-                        self.loginLabel.text = error?.localizedDescription
+                        //Validation successful
+                        self.performSegue(withIdentifier: "goToHome", sender: self) //we use self. because this is inside a closure
                     }
                 }
+                
             case 1: //1 means register
                 Auth.auth().createUser(withEmail: email, password: pass) { (user, error) in
-                    //check user creation is successful
-                    if let userChecked = user {
-                        //Show confirmed creation
-                        self.performSegue(withIdentifier: "goToHome", sender: self) //we use self. because this is inside a closure
+                    //Validation begins here
+                    if (email.count == 0 || pass.count == 0){
+                        self.loginLabel.text = "Email/password fields can't be empty."
+                    } else if error != nil {
+                        //Handle error
+                        if let errCode = AuthErrorCode(rawValue: error!._code) {
+                            switch errCode {
+                            case .invalidEmail:
+                                self.loginLabel.text = "Invalid email."
+                            case .emailAlreadyInUse:
+                                self.loginLabel.text = "Email account already exists."
+                            case .weakPassword:
+                                self.loginLabel.text = "Password is too weak."
+                            default:
+                                print("User Error: \(error!)")
+                            }
+                        }
                     } else {
-                        //Show error
-                        self.loginLabel.text = "Invalid email and password combination."
+                        //Validation successful
+                        self.performSegue(withIdentifier: "goToHome", sender: self) //we use self. because this is inside a closure
                     }
                 }
+                
             default:
                 print("Somehow you chose a button in the selector that doesn't exist!")
             }
-            resetMenu()
-        } else {
-            loginLabel.text = "Email/password can't be empty"
+            //resetMenu()
         }
     }
     
