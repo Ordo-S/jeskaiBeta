afde377980683ae4d7034cd7d6eefb91b3807195
Mark Casapao <casapao.markkenneth@gmail.com>
2018-11-12 19:16:34 -0800

    Added user lockout after 3 attempts
    Users now get locked out after 3 attempts. I'll work on keyboard input types and adding extensions to the code next.

--- AreYouFreeV0.1/AreYouFree/LoginViewController.swift
+++ AreYouFreeV0.1/AreYouFree/LoginViewController.swift
@@ -12,12 +12,20 @@ import Firebase
 
 class LoginViewController: UIViewController {
 
+    //Firebase and widget properties
     var ref: DatabaseReference!
     @IBOutlet weak var loginSelector : UISegmentedControl!
     @IBOutlet weak var loginLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var signInButton: UIButton!
+    
+    //Login lockout properties
+    private var failedLoginAttempts = 0
+    private let failedAttemptsLimit = 3
+    private let lockoutDurationInSeconds = 30
+    private var timerSeconds = 30
+    private var timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -81,6 +89,7 @@ class LoginViewController: UIViewController {
                                 self.loginLabel.text = "Invalid email format."
                             case .wrongPassword:
                                 self.loginLabel.text = "Incorrect password."
+                                self.failedLoginAttempt()
                             case .tooManyRequests:
                                 self.loginLabel.text = "Too many requests. Please wait before retrying."
                             case .networkError:
@@ -133,9 +142,55 @@ class LoginViewController: UIViewController {
         }
     }
     
-    private func resetMenu() {
+    private func resetMenu(){
         emailTextField.text = ""
         passwordTextField.text = ""
+        failedLoginAttempts = 0
+    }
+    
+    private func failedLoginAttempt(){
+        //Count up all the failed attempts and check if it passed the limit
+        failedLoginAttempts += 1
+        if (failedLoginAttempts >= failedAttemptsLimit) {
+            //Disable widgets first
+            failedLoginAttempts = 0
+            loginSelector.isEnabled = false
+            signInButton.isEnabled = false
+            signInButton.backgroundColor = #colorLiteral(red: 0, green: 0.9866840243, blue: 0, alpha: 0.5)
+            emailTextField.isEnabled = false
+            emailTextField.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            passwordTextField.isEnabled = false
+            passwordTextField.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            
+            //Begin timer
+            startTimer()
+        }
+    }
+    
+    //This code is inspired from this source: https://medium.com/ios-os-x-development/build-an-stopwatch-with-swift-3-0-c7040818a10f
+    private func startTimer(){
+        timerSeconds = lockoutDurationInSeconds
+        loginLabel.text = "Too many failed logins. Retry in \(timerSeconds) seconds."
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(LoginViewController.updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc private func updateTimer(){
+        if timerSeconds < 1 {
+            //Turn off timer and re-enable widgets
+            timer.invalidate()
+            loginLabel.text = "Please Sign In."
+            loginSelector.isEnabled = true
+            signInButton.isEnabled = true
+            signInButton.backgroundColor = #colorLiteral(red: 0, green: 0.9866840243, blue: 0, alpha: 1)
+            emailTextField.isEnabled = true
+            emailTextField.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            passwordTextField.isEnabled = true
+            passwordTextField.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            
+        } else {
+            timerSeconds -= 1
+            loginLabel.text = "Too many failed logins. Retry in \(timerSeconds) seconds."
+        }
     }
 }
 
