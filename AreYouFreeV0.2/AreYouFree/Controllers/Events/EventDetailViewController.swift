12a5dca954ba945e9fab02e358f6e61fb106c54c
Ordo <matt@spadarolabs.com>
2018-11-21 19:19:04 -0800

    Event Detail View Shittly set up by matt

---
+++ AreYouFreeV0.2/AreYouFree/Controllers/Events/EventDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EventDetailViewController.swift
+//  
+//
+//  Created by Matt Spadaro on 11/21/18.
+//
+
+import UIKit
+
+class EventDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
