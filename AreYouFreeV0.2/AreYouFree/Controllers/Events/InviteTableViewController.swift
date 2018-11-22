12030deb31bb9daca3382823e079f27cd08ed96e
Ordo <matt@spadarolabs.com>
2018-11-22 10:47:57 -0800

    Cleaned up segue and added in contact selection

--- AreYouFreeV0.2/AreYouFree/Controllers/Events/InviteTableViewController.swift
+++ AreYouFreeV0.2/AreYouFree/Controllers/Events/InviteTableViewController.swift
@@ -60,64 +60,18 @@ class InviteTableViewController: UITableViewController {
         // Return a UI tableView cell
         return cell
     }
-    
-    //MARK: - Navigation
-    
-    // Return to Contacts List.
-    @IBAction func unwindToContactList(segue: UIStoryboardSegue){
-        
-        // We want it to cast as the AddContactViewController as it has text fields!
-        let currentUserID = Singleton.shared.currentUserID
-        
-        // If both text fields are filled (name and username) and it came from AddContactViewController, make a contact!
-        if let viewController = segue.source as? AddContactViewController {
-            guard let name = viewController.nameTextField.text else { return }
-            guard let username = viewController.usernameTextField.text else { return }
-            let contact = Contact(name: name, username: username)
-            // If name and username text fields are filled.
-            if name != "" && username != "" {
-                if let IndexPath = viewController.indexPathForContact {
-                    contacts[IndexPath.row] = contact
-                } else {
-                    // contacts.append(contact)
-                    // here I am adding a new contact to the DB
-                    ref.child(currentUserID + "/Contacts").child(contact.name).setValue(username)
-                }
-            }
-            tableView.reloadData()
-            
-        } else if let viewController = segue.source as? ContactDetailViewController {
-            if viewController.isDeleted{
-                guard let indexPath: IndexPath = viewController.indexPath else { return }
-                let tempContact = contacts[indexPath.row].name  // tempContact is holding the name of the contact we are about to delete
-                contacts.remove(at: indexPath.row)
-                ref.child(currentUserID + "/Contacts").child(tempContact).removeValue() // this removes the contact from our DB
-                
-                tableView.reloadData()
-            }
+    //To select contacts from your list
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCell.AccessoryType.checkmark
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.none
         }
-        
-        
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Makes sure that segue is contactDetailSegue and not AddContact
-        
-        if segue.identifier == "contactDetailSegue" {
-            
-            
-            // if contactDetailSegue is not talking to the ContactDetailViewController... return
-            guard let viewController = segue.destination as? ContactDetailViewController else { return }
-            
-            // Make sure it is unwrapped! If we do not know which row is selected, we won't know which contact to pass on!
-            guard let indexPath = tableView.indexPathForSelectedRow else { return }
-            let contact = contacts[indexPath.row]
-            viewController.contact = contact
-            viewController.indexPath = indexPath
-            
+        else
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.checkmark
         }
     }
-    
     //Orientation lock purposes
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
